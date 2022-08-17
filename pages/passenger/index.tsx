6ce6edab12150aa73dba428ad7604fb7c7af3aad
Deniss Muhļa <deniss.muhla@if.lv>
2022-08-17 17:49:58 +0300

    Add folders

--- pages/passenger/index.tsx
+++ pages/passenger/index.tsx
@@ -13,7 +13,7 @@ const Driver: NextPage = () => {
             </Head>
 
             <main className={styles.main}>
-                <h1 className={styles.title}>Welcome driver!</h1>
+                <h1 className={styles.title}>Welcome passenger!</h1>
             </main>
 
             <footer className={styles.footer}>
