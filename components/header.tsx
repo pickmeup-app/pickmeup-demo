f041b6048bb15edea53f1a123f1f91333de18972
Deniss Muhļa <deniss.muhla@if.lv>
2022-08-17 19:12:25 +0300

    Add header

---
+++ components/header.tsx
@@ -0,0 +1,8 @@
+import { FunctionComponent } from "react";
+import styles from "../styles/common.module.css";
+
+const Header: FunctionComponent = () => {
+    return <div className={styles.header}>header</div>;
+};
+
+export default Header;
